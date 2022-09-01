3e8a726f9d552e56de6ffcd153a4a8a331fe47ba
Dereck Z <zavaladereck77@gmail.com>
2022-09-01 00:09:48 -0600

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,47 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer";
+
+const init = () => {
+    return JSON.parse( localStorage.getItem('todos') ) || []
+}
+
+export const useTodo = () => {
+    const [ todos, dispatch ] = useReducer( todoReducer, [], init )
+    
+    useEffect( () => {
+        localStorage.setItem( 'todos', JSON.stringify( todos ) )
+    }, [ todos ] )
+
+    const handleNewTodo = ( todo ) => {
+        const action = {
+            type: '[TODO] ADD TODO',
+            payload: todo
+        }
+        dispatch( action )
+    }
+
+    const onDeleteTodo = ( id ) => {
+        const action = {
+            type: '[TODO] REMOVE TODO',
+            payload: id
+        }
+        dispatch( action )
+    }
+    
+    const onToggleTodo = ( id ) => {
+        const action = {
+            type: '[TODO] TOGGLE TODO',
+            payload: id
+        }
+        dispatch( action )
+    }
+
+    return {
+        todos,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter(( todo ) => todo.isDone !== true ).length,
+        handleNewTodo,
+        onDeleteTodo,
+        onToggleTodo,
+    }
+}
